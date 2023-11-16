3c21fd8bbfd0e03b95defa2cfa481a2e6c77f5ba
RyanRem <remingtr@oregonstate.edu>
2023-11-15 18:34:57 -0800

    Created banner.  Not ready for PR
    Created the banner for the webpage, but the naming conventions and file structure isn't ready yet.  Just an initial commit for anyone to test it out.

--- src-web-app/tailwind.config.js
+++ src-web-app/tailwind.config.js
@@ -2,7 +2,11 @@
 export default {
   content: ['./src/**/*.{html,js,svelte,ts}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'light-black': '#212529',
+      },
+    },
   },
   plugins: [],
 }
