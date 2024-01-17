e0296085569bc1ca61c665c8965ea057fd0c90c0
fuenteed <114704603+fuenteed@users.noreply.github.com>
2024-01-16 21:28:46 -0800

    Modified the form to retrive data for insertion into the db

--- src-web-app/src/routes/events/create/+page.svelte
+++ src-web-app/src/routes/events/create/+page.svelte
@@ -1,56 +1,47 @@
-<script>
-    import Layout from "../../banner-layout.svelte"
-    import {fly, fade } from 'svelte/transition';
+<script lang='ts'>
+  import Layout from "../../banner-layout.svelte";
+  import { enhance } from '$app/forms';
+  import type { SubmitFunction } from '@sveltejs/kit'; ;
 
-    //variables to store the form input
-    let eventName = '';
-    let eventType = '';
-    let startDate = '';
-    let endDate = '';
-    let description = '';
-  
-    let hasError = false;
-	  let isSuccessVisible = false;
-	  let submitted = false;
-	
-	  const errMessage = "All the fields are mandatory";		
-	
-	  function handleSubmit() {
-		  isSuccessVisible = true;
+  export let data
+  export let form: {
+    eventName?: string;
+    eventType?: string;
+    startDate?: string;
+    endDate?: string;
+    eventDescription?: string;
+  } = {};
 
-		setTimeout(function(){
-			  isSuccessVisible = false;
-		  }, 4000);
-	  }
-    
-    /*
-    function handleSubmit() {
-      // Handle form submission logic here
-      console.log('Event Name:', eventName);
-      console.log('Event Type:', eventType);
-      console.log('Start Date:', startDate);
-      console.log('End Date:', endDate);
-      console.log('Description:', description);
-    }
-    */
+  let { session, supabase, event } = data
+  $: ({ session, supabase, event } = data)
 
-  </script>
-  
- 
-  {#if hasError == true}
-		<p class="error-alert">{errMessage}</p>
-  {:else}
-	  {#if isSuccessVisible}	
-		  <p class="error-alert" transition:fade={{duration:150}}>Data updated successfully</p>
-	  {/if}
-  {/if}
+  let createEventForm: HTMLFormElement
+  let loading = false
+  let eventName: string = event?.event_name ?? ''
+  let eventType: string = event?.event_type ?? ''
+  let startDate: string = event?.event_start_date ?? ''
+  let endDate: string = event?.event_end_date ?? ''
+  let eventDescription: string = event?.event_description ?? ''
 
-  
+  const handleSubmit: SubmitFunction = () => {
+		loading = true
+  	return async () => {
+			loading = false
+		}
+	}
+</script>
+
   <Layout>
     <div class="p-7">
 
-        <form on:submit|preventDefault={handleSubmit} class="flex-1 ml-[16%]">
-      
+      <!--Form Attributes-->
+        <form
+         class= "form-widget flex-1 ml-[16%]" 
+         method= "post"
+         action = "?/events"
+         use:enhance={handleSubmit}
+         bind:this={createEventForm}>
+         
           <div class="flex flex-wrap -mx-3 mb-6">
       
             <!--Header-->
@@ -61,61 +52,61 @@
             <!--Container for Event Name-->
             <div class=" md:w-1/4 px-5 mb-6 md:mb-0">
       
-              <label class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" for="grid-first-name">
+              <label class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" for="eventName">
                 Event Name
               </label>
-              <input required bind:value={eventName} class="appearance-none block w-full bg-gray-200 text-gray-700 border border-black-500 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white" id="grid-first-name" type="text" placeholder="Event Name">
+              <input required name="eventName" id="eventName" value={form?.eventName ?? eventName} class="appearance-none block w-full bg-gray-200 text-gray-700 border border-black-500 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white"  type="text" placeholder="Event Name">
 
             </div>
       
             <!--Container for Event Type Dropdown-->
             <div class=" md:w-2/3 px-5 mb-6 md:mb-0">
 
-              <label class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" for="grid-state">
+              <label class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" for="eventType">
                 Type
               </label>
 
               <div class="relative">
-                <select required bind:value={eventType} class="block appearance-none w-1/4 bg-gray-200 border border-black-500 text-gray-700 py-3 px-4 pr-8 rounded leading-tight focus:outline-none focus:bg-white focus:border-gray-500" id="grid-state">
+                <select required id="eventType" name="eventType" value={form?.eventType ?? eventType} class="block appearance-none w-1/4 bg-gray-200 border border-black-500 text-gray-700 py-3 px-4 pr-8 rounded leading-tight focus:outline-none focus:bg-white focus:border-gray-500">
                   <option>Walk</option>
                   <option>Run</option>
-                  <option>Farting</option>
                 </select>
 
               </div>
             </div>
       
-
             <!--Container for Start Date Input-->
             <div class=" md:w-1/4 px-5 mb-6 md:mb-0">
-              <label class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" for="grid-first-name">
+              <label for="startDate" class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2">
                 Start Date
               </label>
-              <input required bind:value={startDate} class="appearance-none block w-1/2 bg-gray-200 text-gray-700 border border-black-500 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white" id="grid-first-name" type="text" placeholder="MM/DD/YYYY">
+              <input id="startDate" name="startDate" type="date" value={form?.startDate ?? startDate} required class="appearance-none block w-1/2 bg-gray-200 text-gray-700 border border-black-500 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white">
             </div>
       
             <!--Container for End Date Input-->
             <div class=" md:w-1/4 px-5 mb-6 md:mb-0">
-              <label class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" for="grid-first-name">
+              <label for="endDate" class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2">
                 End Date
               </label>
-              <input required bind:value={endDate} class="appearance-none block w-1/2 bg-gray-200 text-gray-700 border border-black-500 rounded py-3 px-6 mb-3 leading-tight focus:outline-none focus:bg-white" id="grid-first-name" type="text" placeholder="MM/DD/YYYY">
+              <input type="date" id="endDate" name="endDate" value={form?.endDate ?? endDate} required class="appearance-none block w-1/2 bg-gray-200 text-gray-700 border border-black-500 rounded py-3 px-6 mb-3 leading-tight focus:outline-none focus:bg-white">
             </div>
             
             <!--Container for Event Description Input-->
             <div class=" md:w-3/4 px-5 mb-6 md:mb-0">
-              <label class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" for="grid-first-name">
+              <label for="eventDescription" class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2">
                 Event Details
               </label>
-              <textarea required bind:value={description} id="message" rows="4" class="block p-2.5 w-1/2 text-sm text-black bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 light:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-black dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Write details about the event here!"></textarea>
+              <textarea id="eventDescription" name="eventDescription" value={form?.eventDescription ?? eventDescription} required rows="4" class="block p-2.5 w-1/2 text-sm text-black bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 light:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-black dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Write details about the event here!"></textarea>
             </div>
       
           </div>
       
-        <div class='form-submit px-2'>
-          <button on:click={() => submitted = true} class="bg-[#D73F09] hover:bg-orange-700 text-white font-bold py-2 p-4 rounded focus:outline-none focus:shadow-outline" type="submit">
-            Create
-          </button>
+        <div class='px-2'>
+          <input
+          type='submit'
+          class="button block primary appearance-none block bg-gray-200 text-gray-700 border border-black-500 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white"
+          value={loading ? 'Loading..' : 'Create Event'}
+          disabled={loading}>
         </div>
       
         </form>
