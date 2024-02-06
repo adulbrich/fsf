5dc44e06d38b8293d617c10be557d48b4ee26a84
RyanRem <remingtr@oregonstate.edu>
2024-02-05 16:18:02 -0800

    Created design for moreEvents
    Created the design but I am planning on creating a route for each event type.  Right now each event type gets directed to the same page.  No live data yet

--- src-web-app/src/routes/events/moreEvents/+page.svelte
+++ src-web-app/src/routes/events/moreEvents/+page.svelte
@@ -34,153 +34,142 @@
 
 <Layout>
 
-    <div class = "flex flex-row ml-[16%] h-[100%]">
-    <!-- Cotnainer for all entities to the right of the navbar -->
-        
-    <div class="flex flex-col w-[100%] h-[100%] mt-[5%]">
-      <!-- Top container that holds searchbar, "Events", search icon, and the create event button -->
-      
-      
-      <div class="flex flex-row h-[75%] w-[90%] ml-[5%] mt-[.5%] custom-border">
-  
-        <!-- Left container that holds the past and ongoing events -->
-        <div class = "flex flex-col h-[90%] w-[50%]  ml-[0%] custom-border">
-          <!-- Ongoing/Past events -->
-          <div class = "flex flex-col w-[90%] ml-[5.9%] h-full">
-            <!-- Ongoing events Label above card -->
-            
-            <!-- Card container -->
-            <div class="flex flex-row w-[100%] drop-shadow-xl pt-3" style="height: 146px">
-                <!-- Image for card -->
-                <div class = "w-[40%] h-full flex-shrink-0">
-                  <img class="h-[100%] w-[100%]" style = "border-top-left-radius: 10px; border-bottom-left-radius: 10px;" src="../../aerial_2.jpg" alt="Scenery">
-                </div>
-                
-                <!-- Text section for card -->
-                <div class="flex flex-col h-full w-[100%] card-border flex-grow-0 overflow-hidden">
-                  <p class = "pt-1 px-2 font-semibold">Walktober</p>
-                  <p class = "pt-1 px-2" style="font-size: 12px;">From the to now</p>
-                  <p class="pt-2 px-2 overflow-hidden" style="font-size: 12px;"> This is a description</p>
-                </div>
-                
-              </div>
-          
-            
-            <!-- Past Events Label above card -->
-            
-            <!-- Card container -->
-            <div class="flex flex-row w-[100%] drop-shadow-xl pt-3" style="height: 146px">
-                <!-- Image for card -->
-                <div class = "w-[40%] h-full flex-shrink-0">
-                  <img class="h-[100%] w-[100%]" style = "border-top-left-radius: 10px; border-bottom-left-radius: 10px;" src="../../aerial_2.jpg" alt="Scenery">
-                </div>
-                
-                <!-- Text section for card -->
-                <div class="flex flex-col h-full w-[100%] card-border flex-grow-0 overflow-hidden">
-                  <p class = "pt-1 px-2 font-semibold">Walktober</p>
-                  <p class = "pt-1 px-2" style="font-size: 12px;">From the to now</p>
-                  <p class="pt-2 px-2 overflow-hidden" style="font-size: 12px;"> This is a description</p>
-                </div>
-                
-              </div>
-            <!-- Second card in past events -->
-            <div class="flex flex-row w-[100%] drop-shadow-xl pt-3" style="height: 146px">
-                <!-- Image for card -->
-                <div class = "w-[40%] h-full flex-shrink-0">
-                  <img class="h-[100%] w-[100%]" style = "border-top-left-radius: 10px; border-bottom-left-radius: 10px;" src="../../aerial_2.jpg" alt="Scenery">
-                </div>
-                
-                <!-- Text section for card -->
-                <div class="flex flex-col h-full w-[100%] card-border flex-grow-0 overflow-hidden">
-                  <p class = "pt-1 px-2 font-semibold">Walktober</p>
-                  <p class = "pt-1 px-2" style="font-size: 12px;">From the to now</p>
-                  <p class="pt-2 px-2 overflow-hidden" style="font-size: 12px;"> This is a description</p>
-                </div>
-                
-              </div>
-           
-            
-          
-          </div>
-        </div>
-        <!-- Left container that holds the past and ongoing events -->
-        <div class = "flex flex-col h-[90%] w-[50%]  ml-[0%] custom-border">
-            <!-- Ongoing/Past events -->
-            <div class = "flex flex-col w-[90%] ml-[5.9%] h-full">
-              <!-- Ongoing events Label above card -->
-              
-              <!-- Card container -->
-              <div class="flex flex-row w-[100%] drop-shadow-xl pt-3" style="height: 146px">
-                <!-- Image for card -->
-                <div class = "w-[40%] h-full flex-shrink-0">
-                  <img class="h-[100%] w-[100%]" style = "border-top-left-radius: 10px; border-bottom-left-radius: 10px;" src="../../aerial_2.jpg" alt="Scenery">
-                </div>
-                
-                <!-- Text section for card -->
-                <div class="flex flex-col h-full w-[100%] card-border flex-grow-0 overflow-hidden">
-                  <p class = "pt-1 px-2 font-semibold">Walktober</p>
-                  <p class = "pt-1 px-2" style="font-size: 12px;">From the to now</p>
-                  <p class="pt-2 px-2 overflow-hidden" style="font-size: 12px;"> This is a description</p>
-                </div>
-                
-              </div>
-            
-              
-              <!-- Past Events Label above card -->
-              
-              <!-- Card container -->
-              <div class="flex flex-row w-[100%] drop-shadow-xl pt-3" style="height: 146px">
-                <!-- Image for card -->
-                <div class = "w-[40%] h-full flex-shrink-0">
-                  <img class="h-[100%] w-[100%]" style = "border-top-left-radius: 10px; border-bottom-left-radius: 10px;" src="../../aerial_2.jpg" alt="Scenery">
-                </div>
-                
-                <!-- Text section for card -->
-                <div class="flex flex-col h-full w-[100%] card-border flex-grow-0 overflow-hidden">
-                  <p class = "pt-1 px-2 font-semibold">Walktober</p>
-                  <p class = "pt-1 px-2" style="font-size: 12px;">From the to now</p>
-                  <p class="pt-2 px-2 overflow-hidden" style="font-size: 12px;"> This is a description</p>
-                </div>
-                
-              </div>
-              
-              <!-- Second card in past events -->
-              <div class="flex flex-row w-[100%] drop-shadow-xl pt-3" style="height: 146px">
-                <!-- Image for card -->
-                <div class = "w-[40%] h-full flex-shrink-0">
-                  <img class="h-[100%] w-[100%]" style = "border-top-left-radius: 10px; border-bottom-left-radius: 10px;" src="../../aerial_2.jpg" alt="Scenery">
-                </div>
-                
-                <!-- Text section for card -->
-                <div class="flex flex-col h-full w-[100%] card-border flex-grow-0 overflow-hidden">
-                  <p class = "pt-1 px-2 font-semibold">Walktober</p>
-                  <p class = "pt-1 px-2" style="font-size: 12px;">From the to now</p>
-                  <p class="pt-2 px-2 overflow-hidden" style="font-size: 12px;"> This is a description</p>
-                </div>
-                
-              </div>
-             
-              
-            
-            </div>
-          </div>
-        
+  <div class="flex flex-row justify-between items-center ml-[16%] w-[20%] mt-3">
+    <button style = "font-size: 10px" class="flex items-center bg-beaver-orange ml-[3%] mt-1 h-6 hover:bg-dark-black text-white py-2 px-4 rounded-full">
+      <a href="/events" class="flex items-center text-white">
+        Back
+    </a>
+    </button>
+    <h1 class="event-word mr-36">Events</h1>
     
-        
+    
+    </div>
+  <!-- Container for Events -->
+  <div class="container ml-[16%] w-auto h-[75%] mt-8 flex flex-wrap justify-between">
+    
+    <!-- ROW 1 COL 1 -->
+    <div class="w-1/3 pt-1 custom-border ml-36 drop-shadow-xl flex flex-column 1/8">
+      <div class = "w-[40%] h-full">
+        <img class="h-[100%] w-[100%]" style = "border-top-left-radius: 10px; border-bottom-left-radius: 10px;" src="../../aerial_2.jpg" alt="Scenery">
       </div>
-      <!-- Container that holds page numbers-->
-    <div class="flex flex-row ml-[45%] custom-border" style="margin-bottom: 15px; margin-top: 20px">
-        <div class="mb-0 mx-1 w-10 h-10 bg-beaver-orange rounded-full flex items-center justify-center text-white">1</div>
-        <div class="mb-0 mx-1 w-10 h-10 bg-light-black rounded-full flex items-center justify-center text-white">2</div>
-        <div class=" mb-0 mx-1 w-10 h-10 bg-light-black rounded-full flex items-center justify-center text-white">3</div>
-    </div>
+      
+      <!-- Text section for card -->
+      <div class="flex flex-col w-2/3 card-border overflow-hidden h-full">
+        <p class = "pt-1 px-2 font-semibold">Walktober</p>
+        <p class = "pt-1 px-2" style="font-size: 12px;">From the to now</p>
+        <p class="pt-2 px-2 overflow-hidden" style="font-size: 12px;"> This is a description</p>
+      </div>
     </div>
     
+    <!-- ROW 1 COL 2 -->
+    <div class="w-1/3 pt-1 custom-border mr-36 drop-shadow-xl flex flex-column 1/8">
+      <div class = "w-[40%] h-full">
+        <img class="h-[100%] w-[100%]" style = "border-top-left-radius: 10px; border-bottom-left-radius: 10px;" src="../../aerial_2.jpg" alt="Scenery">
+      </div>
+      
+      <!-- Text section for card -->
+      <div class="flex flex-col w-2/3 card-border overflow-hidden h-full">
+        <p class = "pt-1 px-2 font-semibold">Walktober</p>
+        <p class = "pt-1 px-2" style="font-size: 12px;">From the to now</p>
+        <p class="pt-2 px-2 overflow-hidden" style="font-size: 12px;"> This is a description</p>
+      </div>
+    </div>
   
+    <!-- ROW 2 COL 1 -->
+    <div class="w-1/3 pt-1 custom-border ml-36 drop-shadow-xl flex flex-column 1/8">
+      <div class = "w-[40%] h-full">
+        <img class="h-[100%] w-[100%]" style = "border-top-left-radius: 10px; border-bottom-left-radius: 10px;" src="../../aerial_2.jpg" alt="Scenery">
+      </div>
+      
+      <!-- Text section for card -->
+      <div class="flex flex-col w-2/3 card-border overflow-hidden h-full">
+        <p class = "pt-1 px-2 font-semibold">Walktober</p>
+        <p class = "pt-1 px-2" style="font-size: 12px;">From the to now</p>
+        <p class="pt-2 px-2 overflow-hidden" style="font-size: 12px;"> This is a description</p>
+      </div>
+    </div>
+    
+    <!-- ROW 2 COL 2 -->
+    <div class="w-1/3 custom-border pt-1 mr-36 drop-shadow-xl flex flex-column 1/8">
+      <div class = "w-[40%] h-full">
+        <img class="h-[100%] w-[100%]" style = "border-top-left-radius: 10px; border-bottom-left-radius: 10px;" src="../../aerial_2.jpg" alt="Scenery">
+      </div>
+      
+      <!-- Text section for card -->
+      <div class="flex flex-col w-2/3 card-border overflow-hidden h-full">
+        <p class = "pt-1 px-2 font-semibold">Walktober</p>
+        <p class = "pt-1 px-2" style="font-size: 12px;">From the to now</p>
+        <p class="pt-2 px-2 overflow-hidden" style="font-size: 12px;"> This is a description</p>
+      </div>
+    </div>
   
-  
-  
+    <!-- ROW 3 COL 1 -->
+    <div class="w-1/3 custom-border pt-1 ml-36 drop-shadow-xl flex flex-column 1/8">
+      <div class = "w-[40%] h-full">
+        <img class="h-[100%] w-[100%]" style = "border-top-left-radius: 10px; border-bottom-left-radius: 10px;" src="../../aerial_2.jpg" alt="Scenery">
+      </div>
+      
+      <!-- Text section for card -->
+      <div class="flex flex-col pt-1 w-2/3 card-border overflow-hidden h-full">
+        <p class = "pt-1 px-2 font-semibold">Walktober</p>
+        <p class = "pt-1 px-2" style="font-size: 12px;">From the to now</p>
+        <p class="pt-2 px-2 overflow-hidden" style="font-size: 12px;"> This is a description</p>
+      </div>
+    </div>
+    <!-- ROW 3 COL 2 -->
+    <div class="w-1/3 pt-1 custom-border mr-36 drop-shadow-xl flex flex-column 1/8">
+      <div class = "w-[40%] h-full">
+        <img class="h-[100%] w-[100%]" style = "border-top-left-radius: 10px; border-bottom-left-radius: 10px;" src="../../aerial_2.jpg" alt="Scenery">
+      </div>
+      
+      <!-- Text section for card -->
+      <div class="flex flex-col w-2/3 card-border overflow-hidden h-full">
+        <p class = "pt-1 px-2 font-semibold">Walktober</p>
+        <p class = "pt-1 px-2" style="font-size: 12px;">From the to now</p>
+        <p class="pt-2 px-2 overflow-hidden" style="font-size: 12px;"> This is a description</p>
+      </div>
+    </div>
+
+    <!-- ROW 4 COL 2 -->
+    <div class="w-1/3 pt-1 custom-border ml-36 drop-shadow-xl flex flex-column 1/8">
+      <div class = "w-[40%] h-full">
+        <img class="h-[100%] w-[100%]" style = "border-top-left-radius: 10px; border-bottom-left-radius: 10px;" src="../../aerial_2.jpg" alt="Scenery">
+      </div>
+      
+      <!-- Text section for card -->
+      <div class="flex flex-col w-2/3 card-border overflow-hidden h-full">
+        <p class = "pt-1 px-2 font-semibold">Walktober</p>
+        <p class = "pt-1 px-2" style="font-size: 12px;">From the to now</p>
+        <p class="pt-2 px-2 overflow-hidden" style="font-size: 12px;"> This is a description</p>
+      </div>
+    </div>
+    <!-- ROW 4 COL 2 -->
+    <div class="w-1/3 pt-1 custom-border mr-36 drop-shadow-xl flex flex-column 1/8">
+      <div class = "w-[40%] h-full">
+        <img class="h-[100%] w-[100%]" style = "border-top-left-radius: 10px; border-bottom-left-radius: 10px;" src="../../aerial_2.jpg" alt="Scenery">
+      </div>
+      
+      <!-- Text section for card -->
+      <div class="flex flex-col w-2/3 card-border overflow-hidden h-full">
+        <p class = "pt-1 px-2 font-semibold">Walktober</p>
+        <p class = "pt-1 px-2" style="font-size: 12px;">From the to now</p>
+        <p class="pt-2 px-2 overflow-hidden" style="font-size: 12px;"> This is a description</p>
+      </div>
+    </div>
   </div>
+ 
+    
+    
+    
+      
+  
+  <!-- Container that holds page numbers-->
+  <div class="flex justify-between ml-[47.85%] custom-border w-1/5" style="margin-bottom: 15px; margin-top: 20px">
+    <div class="mb-0 ml-20 w-10 h-10 bg-beaver-orange rounded-full flex items-center justify-center text-white">1</div>
+    <div class="mb-0 mx-1 w-10 h-10 bg-light-black rounded-full flex items-center justify-center text-white">2</div>
+    <div class=" mb-0 mr-20 w-10 h-10 bg-light-black rounded-full flex items-center justify-center text-white">3</div>
+  </div>
+   
 
 
 </Layout>
@@ -200,6 +189,12 @@
       border-top-right-radius: 10px;
       border-bottom-right-radius: 10px;
   
+    }
+    .custom-border {
+      /* box-sizing: border-box;
+      background: #FFFFFF; 
+      border: 0.5px solid #c7c7cd;
+      border-radius: 10px;  */
     }
     
   </style>
