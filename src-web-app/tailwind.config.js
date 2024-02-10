d1f7c3c85d3e55be12201ff57d8116fb6b95290f
RyanRem <remingtr@oregonstate.edu>
2024-02-09 19:49:29 -0800

    Removed dark theme

--- src-web-app/tailwind.config.js
+++ src-web-app/tailwind.config.js
@@ -20,7 +20,7 @@ export default {
   
   daisyui: {
     themes: false, // false: only light + dark | true: all themes | array: specific themes like this ["light", "dark", "cupcake"]
-    darkTheme: "dark", // name of one of the included themes for dark mode
+    darkTheme: false, // name of one of the included themes for dark mode
     base: false, // applies background color and foreground color for root element by default
     styled: true, // include daisyUI colors and design decisions for all components
     utils: true, // adds responsive and modifier utility classes
