65d5777a300a5807d0782fbbc561f9919d29cbf6
fuenteed <114704603+fuenteed@users.noreply.github.com>
2024-02-22 21:13:37 -0800

    Add pages to overview including teams, teams/members/ , participants, and achievments

---
+++ src-web-app/src/routes/events/[EventID]/Achievments/+page.svelte
@@ -0,0 +1,30 @@
+<script>
+    export let data;
+    import Layout from '../../../banner-layout.svelte';
+</script>
+
+
+<svelte:head>
+   <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.4.0/css/font-awesome.min.css">
+
+    <title>
+           Event | Top Teams & Participants
+    </title>
+</svelte:head>
+
+<style type="text/css">
+.fa{
+color: 
+#D73F09
+}
+</style>
+
+<Layout>
+
+    <div class='container ml-[16%]'>
+
+        Achievements
+
+    </div>
+
+</Layout>
