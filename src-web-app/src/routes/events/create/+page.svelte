5b85ef2da91b4d7231a5ef40cb4291d555fa7a0e
Ryan <remingtr@oregonstate.edu>
2024-03-03 00:57:55 -0800

    Changed Design for Create Event
    Modified the design for create event

--- src-web-app/src/routes/events/create/+page.svelte
+++ src-web-app/src/routes/events/create/+page.svelte
@@ -40,7 +40,7 @@
     <form class="form-widget flex-1 ml-[17%]" method="post" action="?/create" use:enhance={handleSubmit} bind:this={createEventForm}>
       <div class="flex flex-wrap -mx-3 mb-6">
         <a href="/events" style="margin-left: 10px; margin-top:15px;">
-          <!-- Arrow that redirects to /events -->
+          <!-- <button style="width:60px;"class="btn btn-xs font-normal font-sans bg-light-black hover:bg-light-blackSelected text-white rounded-full">Back</button> -->
           <svg width="20" height="17" viewBox="0 0 51 43" fill="none" xmlns="http://www.w3.org/2000/svg">
             <path d="M24.1251 42.4336C23.2836 42.4243 22.4765 42.0987 21.8646 41.5217L0.911133 21.1135L21.8646 0.705341C22.5076 0.308915 23.2685 0.147891 24.0172 0.249796C24.7659 0.351701 25.456 0.710211 25.9694 1.26403C26.4827 1.81785 26.7876 2.5326 26.8318 3.28607C26.876 4.03954 26.6568 4.78496 26.2117 5.39489L10.3011 21.1135L26.4726 36.8322C26.9461 37.2876 27.2708 37.8755 27.404 38.5185C27.5371 39.1616 27.4725 39.8298 27.2186 40.4356C26.9648 41.0413 26.5335 41.5563 25.9815 41.9128C25.4294 42.2694 24.7823 42.4509 24.1251 42.4336Z" fill="black"/>
             <path d="M47.5996 24.3699H9.25746C8.39275 24.3699 7.56346 24.0268 6.95202 23.4161C6.34057 22.8054 5.99707 21.977 5.99707 21.1133C5.99707 20.2496 6.34057 19.4213 6.95202 18.8105C7.56346 18.1998 8.39275 17.8567 9.25746 17.8567H47.5996C48.4643 17.8567 49.2936 18.1998 49.9051 18.8105C50.5165 19.4213 50.86 20.2496 50.86 21.1133C50.86 21.977 50.5165 22.8054 49.9051 23.4161C49.2936 24.0268 48.4643 24.3699 47.5996 24.3699V24.3699Z" fill="black"/>
@@ -85,8 +85,7 @@
               <option>Walk (Steps)</option>
               <option>Run (Distance)</option>
             </select>
-            <!-- Drop down arrow  -->
-            <div class="absolute" style="left: 20.8%; bottom:12px; pointer-events:none;">
+            <div class="absolute" style="left: 165px; bottom:12px; pointer-events:none;">
               <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
                 <path d="M6 9L12 15L18 9" stroke="black" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
                 </svg>
