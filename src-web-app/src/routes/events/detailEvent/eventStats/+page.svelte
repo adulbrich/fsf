aab42b14aeaf75498809856b6455e0e17106f941
Ryan <remingtr@oregonstate.edu>
2024-03-04 16:50:40 -0800

    Created a chart from frappe-charts

---
+++ src-web-app/src/routes/events/detailEvent/eventStats/+page.svelte
@@ -0,0 +1,19 @@
+<script>
+    import Chart from 'svelte-frappe-charts';
+  
+    let data = {
+      labels: Array.from({ length: 31 }, (_, i) => i + 1),
+      datasets: [
+        {
+            name: 'Active Users', values: Array.from({ length: 31 }, (_, i) => Math.floor(Math.random() * (500 - 100 + 1)) + 100),
+
+        },
+        {
+            name: 'Inactive Users', values: Array.from({ length: 31 }, (_, i) => Math.floor(Math.random() * (500 - 100 + 1)) + 100),
+        }
+      ]
+    };
+  </script>
+  <div class="mt-[20%] w-[90%] ml-[5%]" >
+    <Chart data={data} type="line" />
+  </div>
