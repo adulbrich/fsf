2f6ae99580a1be28a6c1bee290760f1194b3deb8
jason-t-pho <jtanho02@gmail.com>
2024-03-15 19:32:48 -0700

    Made changes to UI on page

--- src-web-app/src/routes/events/[EventID]/Overview/+page.svelte
+++ src-web-app/src/routes/events/[EventID]/Overview/+page.svelte
@@ -45,41 +45,32 @@
 
            <!--Container for Left Side (Event Details)-->
 
-            <div class=' text-center py-5'>
+            <div class='text-left py-5'>
               <!--Event Title and Description-->
               <div >
-                <h2 class='font-bold text-xl'>A little about {data.eventDetails?.Name}!</h2>
+                <h2 class='font-bold text-xl'>A little about: {data.eventDetails?.Name}!</h2>
               </div>
 
               <p class='py-3 px-3 leading-loose'>{data.eventDetails.Description}</p>
 
-
                 <!--Start and End Dates-->
-               <div class='py-3 flex justify-around'>
-                  <div>
-                    <span class='font-bold py-3'>Start date</span>
-                    <div class='py-3'>
-                      {data.eventDetails?.StartsAt.slice(0,10)}
-                    </div>
-                  </div>
+               <div class='py-1'>
+                <span class='font-bold py-3 text-xl'>Date and Time</span>
 
-                  <div> 
-                    <span class='font-bold py-3'>End date</span>
-                    <div class='py-3'>
-                      {data.eventDetails?.EndsAt.slice(0,10)}
-                    </div>
-                  </div>
                </div>
+                <div class='py-1 leading-loose'>
+                  <i class="fa fa-calendar fa-1x"></i>  {data.eventDetails?.StartsAt.slice(0,10)} -- {data.eventDetails?.EndsAt.slice(0,10)}
+                </div>
 
 
                <!--What are partipants collecting-->
                <div class='py-5'>
 
                 <div>
-                  <h2 class='font-bold'>What are partipants collecting?</h2>
+                  <h2 class='font-bold text-xl'>What to Expect</h2>
 
-                  <div class='py-3'>
-                    <span class='font-semibold'>Leaves</span> <i class="fa fa-leaf fa-2x"></i></div>
+                  <div class='py-1'>
+                    <span class='font-semi'>Daily {data.eventDetails?.Type} Progress Converts to Leaves</span> <i class="fa fa-leaf fa-2x"></i></div>
                   </div>
 
                </div>
@@ -87,23 +78,24 @@
 
                <!--Daily Tiers-->
               <div class='py-9'>
-                <h2 class='font-bold'>Daily Tiers</h2>
+                <h2 class='font-bold text-xl'>Daily Tiers</h2>
 
-                <div class='py-3 flex justify-around'>
+                <div class='py-3'>
 
-                  <div>3000 <i class="fa fa-leaf"></i></div>
-                  <div>6000 <i class="fa fa-leaf"></i></div>
-                  <div>12,000 <i class="fa fa-leaf"></i></div>
+                  <div>Level 1 - 3000 <i class="fa fa-leaf"></i></div>
+                  <div>Level 2 - 6000 <i class="fa fa-leaf"></i></div>
+                  <div>Level 3 - 12,000 <i class="fa fa-leaf"></i></div>
 
                 </div>
               </div>
 
-
+              <button>Edit Event</button>
             </div>
 
             <!--Container for right side (Event Picture)-->
             <div class="flex justify-center items-center">
-              <img class="object-cover w-80 h-80 md:w-110 md:h-110 rounded-full" src={data.testsigned?.signedUrl} />
+              <!-- <img class="object-cover w-80 h-80 md:w-110 md:h-110 rounded-full" src={data.testsigned?.signedUrl} /> -->
+              <img class="object-cover w-80 h-80 md:w-110 md:h-110 rounded-full" src='http://placekitten.com/g/200/300' />
             </div>
 
           </div>
