f71af6d6131012bcd4ccc789aac17fc7bbecad15
jason-t-pho <jtanho02@gmail.com>
2024-04-28 15:08:41 -0500

    Starter code for reset mobile

---
+++ src-mobile-app/src/features/password-reset/ForgotPassword.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+import { router } from "expo-router";
+import { Button, Input, Separator, XStack, YStack } from 'tamagui';
+
+export function ForgotPasswordForm() {
+    return (
+        <YStack>
+
+        </YStack>
+    )
+}
