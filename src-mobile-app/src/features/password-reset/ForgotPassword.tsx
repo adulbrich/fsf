9cd2c8bb6779cfe8ebee3a68864b410c1de30dfe
jason-t-pho <jtanho02@gmail.com>
2024-04-29 15:38:25 -0500

    Added forms for password reset

--- src-mobile-app/src/features/password-reset/ForgotPassword.tsx
+++ src-mobile-app/src/features/password-reset/ForgotPassword.tsx
@@ -1,11 +1,50 @@
-import React from "react";
+import React, { useState } from "react";
 import { router } from "expo-router";
 import { Button, Input, Separator, XStack, YStack } from 'tamagui';
+import { Mail } from "@tamagui/lucide-icons";
+
 
 export function ForgotPasswordForm() {
-    return (
-        <YStack>
 
+    const [email, setEmail] = useState('')
+    const [emailFocus, setEmailFocus] = useState(false);
+
+    const sendPasswordEmail = React.useCallback(() => {
+        console.log("send that email")
+    }, []);
+
+    return (
+        <YStack flex={1}>
+             <XStack width={"100%"} justifyContent="flex-start" alignItems="center">
+        
+                <YStack flex={1}>
+                <Input
+                    id="email"
+                    onChangeText={(text) => setEmail(text)}
+                    value={email}
+                    placeholder="Email"
+                    autoCapitalize={'none'}
+                    height={"$5"}
+                    paddingLeft={"$9"}
+                    borderWidth={1}
+                    borderBottomWidth={3}
+                    focusStyle={{
+                    borderColor: '#006A8E55'
+                    }}
+                    onFocus={() => setEmailFocus(true)}
+                    onBlur={() => setEmailFocus(false)}
+                    />
+                </YStack>
+
+                <YStack justifyContent="center" position="absolute" left={"$3"}>
+                <Mail color={"$gray8"} />
+                </YStack>
+
+            </XStack>
+            
+            <Separator />
+
+            <Button onPress={sendPasswordEmail} marginTop="$2" height={"$5"} color={"white"} borderColor={"black"} borderWidth={2} backgroundColor={"black"}>Send Email</Button>
         </YStack>
     )
 }
