826bdbf1575296bc9aa56e7c067dfd9e857ab1d7
fuenteed <114704603+fuenteed@users.noreply.github.com>
2024-04-30 23:31:59 -0700

    Finished Initial Version of Home Page

--- src-mobile-app/src/app/(tabs)/home.tsx
+++ src-mobile-app/src/app/(tabs)/home.tsx
@@ -1,11 +1,13 @@
 import { useSelector } from "react-redux";
-import { YStack, useTheme, H4, XStack, ScrollView, H3, Text, View } from "tamagui";
+import { Button, YStack, useTheme, H4, XStack, ScrollView, H3, H2, Text, View, CardProps, Card , Image} from "tamagui";
 import { fetchTeamStatsBreakdown, selectMyTeamStats } from "../../store/teamStatsSlice";
-import { SafeAreaView } from "react-native";
+import { SafeAreaView, StyleSheet} from "react-native";
 import { Text as RN_Text } from "react-native";
 import { useTypedDispatch, useTypedSelector } from "../../store/store";
 import { selectEvents } from "../../store/eventsSlice";
 import { useEffect } from "react";
+import { useLocalSearchParams, Stack } from "expo-router";
+import { useAssets } from "expo-asset";
 
 export default function Home() {
   const theme = useTheme();
@@ -21,30 +23,73 @@ export default function Home() {
       ...ts,
       Event: events.find(ev => ev.EventID === ts.EventID)
   }));
+  
+  const [assets] = useAssets([
+    require('../../../assets/images/preview_wide.jpg')
+  ]);
 
   return (
     <SafeAreaView>
       <ScrollView height={'100%'} padding="$4">
-        <YStack justifyContent="center" alignItems="center" flex={1} gap="$4">
-      
+        <YStack justifyContent="center" alignItems="center" flex={1} gap="$4" marginBottom="$4">
+          
           {vm.map(ts => (
-            <YStack width={'100%'} paddingHorizontal="0" key={ts.TeamID} gap="$4">
-              <H3>{ts.Event?.Name}</H3>
+            <Card
+             animation="bouncy"
+             size="$3"
+             width={'95%'}
+             height={350}
+             scale={0.9}
+             pressStyle={{scale: 0.875}} 
+             backgroundColor={theme.gray5}
+             borderColor={theme.gray6}
+             padding="$5"
+             borderRadius={15}
+             key={ts.TeamID}>
+
+            <YStack flex={1}>
+                <Card.Header><H3 style={{textAlign: 'center'}}>{ts.Event?.Name}</H3></Card.Header>
+                {assets && (
+
+                    <Image
+                      width={'95%'}
+                      height={'50%'}
+                      resizeMode="stretch"
+                      borderRadius={10}
+                      alignSelf="center"
+                      marginBottom="$5"
+                      source={{ uri: assets[0].uri, width: assets[0].width!, height: assets[0].height! }}
+                    />
+
+                )}
+            </YStack>
+          
+            <YStack height={100} gap="$3" marginBottom="$4">
               <XStack justifyContent="space-between" alignItems="flex-end">
                 <H4>{ts.TeamName}</H4>
                 <H4>{ts.TotalScore}</H4>
               </XStack>
+
               <View width={'100%'} height={1} backgroundColor={'$gray10'} />
-              {ts.Profiles.map(ps => (
-                <XStack justifyContent="space-between" key={ps.ProfileID} alignItems="flex-end">
-                  <Text>{ps.ProfileName}</Text>
-                  <Text>{ps.TotalScore}</Text>
-                </XStack>
-              ))}
+                
+                {ts.Profiles.slice(0,2).map(ps => (
+                  <XStack justifyContent="space-between" key={ps.ProfileID} alignItems="flex-end">
+                    <Text>{ps.ProfileName}</Text>
+                    <Text>{ps.TotalScore}</Text>
+                  </XStack>
+                ))}
+                
+              {ts.Profiles.length > 2 && <Text>...</Text>}
+
             </YStack>
+
+
+            </Card>
           ))}
+
         </YStack>
       </ScrollView>
     </SafeAreaView>
   );
+
 }
