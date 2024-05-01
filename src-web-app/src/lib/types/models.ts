9da71a45c70851ed85614e0d8d03d857e703b0a0
Stephen <teamgreensf@gmail.com>
2024-05-01 01:14:58 -0700

    Remove Event ambiguity by calling events SBEvent

---
+++ src-web-app/src/lib/types/models.ts
@@ -0,0 +1,11 @@
+// Blue print for the event object
+export interface SBEvent {
+  Name: string;
+  Type: string;
+  StartsAt: string;
+  EndsAt: string;
+  Status: string;
+  Description: string;
+  Exists: boolean;
+  EventID: string;
+}
