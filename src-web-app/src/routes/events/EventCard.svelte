f3053512a7dcc45ee6aa66d5f5ba883b8fcaeeb5
Stephen <teamgreensf@gmail.com>
2024-05-01 01:04:32 -0700

    Remove redundant component

--- src-web-app/src/routes/events/EventCard.svelte
+++ src-web-app/src/routes/events/EventCard.svelte
@@ -2,29 +2,32 @@
 <!-- This is the card component that will be used to display the events in the events page -->
 
 <script lang="ts">
-  import type { SBEvent } from "$lib/types/models";
-  export let event: SBEvent | null = null;
+  // Props
+  export let Name: string = "";
+  export let StartsAt: string = "";
+  export let EndsAt: string = "";
+  export let Description: string = "";
+  export let EventID: string = ""; 
+
+  export let index = 1;
 </script>
-
-{#if event}
-  <!-- Clicking on this event card takes you to a detailed event view -->
-  <a href="/events/{event.EventID}/Overview" class="inline-flex h-32">
-    <div class="ring-1 ring-black/10 rounded-lg overflow-hidden shadow-md flex">
-      <!-- Event Banner  -->
-      <div class="basis-2/5 h-full">
-        <img class="w-full h-full" src="../../aerial_2.jpg" alt="Scenery" />
-      </div>
-
-      <!-- Text section for card -->
-      <div class="basis-3/5 flex flex-col">
-        <p class="pt-1 px-2 font-semibold">{event.Name}</p>
-        <p class="pt-1 px-2 text-xs">
-          From {event.StartsAt} to {event.EndsAt}
-        </p>
-        <p class="pt-2 px-2 overflow-hidden text-sm">
-          {event.Description}
-        </p>
-      </div>
+<!-- Clicking on this event card takes you to a detailed event view -->
+<a href="/events/{EventID}/Overview" class="inline-flex h-32">
+  <div class="ring-1 ring-black/10 rounded-lg shadow-md flex flex-column grow-0" class:mr-[10%]={index % 2 != 0} class:ml-[10%]={index % 2 == 0}>
+    <!-- Event Banner  -->
+    <div class="w-[40%] h-full">
+      <img class="h-[100%] w-[100%]" style="border-top-left-radius: 10px; border-bottom-left-radius: 10px;" src="../../aerial_2.jpg" alt="Scenery" />
     </div>
-  </a>
-{/if}
+
+    <!-- Text section for card -->
+    <div class="flex flex-col w-2/3 overflow-hidden h-full grow-0 shrink-0">
+      <p class="pt-1 px-2 font-semibold">{Name}</p>
+      <p class="pt-1 px-2" style="font-size: 12px;">
+        From {StartsAt} to {EndsAt}
+      </p>
+      <p class="pt-2 px-2 overflow-hidden" style="font-size: 12px;">
+        {Description}
+      </p>
+    </div>
+  </div>
+</a>
