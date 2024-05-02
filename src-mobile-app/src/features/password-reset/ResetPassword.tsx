e4530be6a0abaf27cb676ad2701898fec4e13d3b
jason-t-pho <jtanho02@gmail.com>
2024-05-01 19:32:16 -0500

    Finalized password reset pages

--- src-mobile-app/src/features/password-reset/ResetPassword.tsx
+++ src-mobile-app/src/features/password-reset/ResetPassword.tsx
@@ -92,9 +92,7 @@ export default function ResetPasswordForm() {
 
             <Button onPress={resetPassword} marginTop="$2" height={"$5"} color={"white"} borderColor={"black"} borderWidth={2} backgroundColor={"black"}>Reset Password</Button>
 
-            
-
-            </XStack>
+        
         </YStack>
     )
 }
