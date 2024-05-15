8e2ae06e740b5598209fb849e66d34a99b90287c
Stephen <teamgreensf@gmail.com>
2024-05-15 16:34:46 -0700

    Landing page on `/about`

---
+++ src-web-app/src/routes/about/+page.svelte
@@ -0,0 +1,58 @@
+
+<div class="flex flex-col items-center w-full gap-8 my-16">
+  <!-- Header -->
+  <div class="flex text-center gap-16 items-center">
+    <div class="flex flex-col text-left font-serif">
+      <div class="text-4xl">Oregon State University</div>
+      <div class="text-3xl">Faculty Staff Fitness</div>
+    </div>
+    <div class="text-xl italic">Bringing you the next generation of fitness tracking</div>
+  </div>
+
+  <!-- Screenshot -->
+  <div class="max-w-screen-md pointer-events-none">
+    <img alt="screenshot" src="/about/web_screenshot.png" />
+  </div>
+
+  <!-- Call to action -->
+  <div class="my-8 mt-4">
+    <div class="px-6 py-3 rounded-full text-2xl ring-2 ring-orange-400 hover:bg-orange-400 hover:text-white hover:scale-105 transition cursor-pointer">Get started today</div>
+  </div>
+
+  <!-- Features header -->
+  <div class="text-3xl">
+    <div>Designed to keep you moving!</div>
+  </div>
+
+  <!-- Features (double column) -->
+  <div class="flex gap-8">
+    <div class="flex flex-col gap-8">
+      <div class="flex gap-4 items-center">
+        <div class="font-bold">Track your progress</div>
+      </div>
+      <div class="flex gap-4 items-center">
+        <div class="font-bold">Set goals</div>
+      </div>
+      <div class="flex gap-4 items-center">
+        <div class="font-bold">Stay motivated</div>
+      </div>
+      <div class="flex gap-4 items-center">
+        <div class="font-bold">Share with friends</div>
+      </div>
+    </div>
+    <div class="flex flex-col gap-8">
+      <div class="flex gap-4 items-center">
+        <div>Keep track of your workouts and see your progress over time.</div>
+      </div>
+      <div class="flex gap-4 items-center">
+        <div>Set goals for yourself and track your progress towards them.</div>
+      </div>
+      <div class="flex gap-4 items-center">
+        <div>Stay motivated with reminders and notifications.</div>
+      </div>
+      <div class="flex gap-4 items-center">
+        <div>Share your progress with friends and family.</div>
+      </div>
+    </div>
+  </div>
+</div>
