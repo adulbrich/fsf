dbbf223b6255ce07c4051a3d9f08ca7f1d830036
jason-t-pho <jtanho02@gmail.com>
2024-05-17 15:21:39 -0700

    Added more FAQ Questions on about page

--- src-web-app/src/routes/about/Accordion.svelte
+++ src-web-app/src/routes/about/Accordion.svelte
@@ -11,23 +11,23 @@
   </div>
   <div class="collapse collapse-arrow join-item border border-base-300">
     <input type="radio" name="my-accordion-4" />
-    <div class="collapse-title text-xl font-medium">Add some questions here</div>
+    <div class="collapse-title text-xl font-medium">Do I have to participate in all events?</div>
     <div class="collapse-content">
-      <p>Answer</p>
+      <p>Our application allows users to follow the standard FSF event sign-up procedure. You are able to sign-up for specific events you want to participate in!</p>
     </div>
   </div>
   <div class="collapse collapse-arrow join-item border border-base-300">
     <input type="radio" name="my-accordion-4" />
-    <div class="collapse-title text-xl font-medium">Add some questions here</div>
+    <div class="collapse-title text-xl font-medium">Is there any payment necessary?</div>
     <div class="collapse-content">
-      <p>Answer</p>
+      <p>There is no necessary payment to use our application. When you sign-up for events, you will be prompted if there is any required transaction.</p>
     </div>
   </div>
   <div class="collapse collapse-arrow join-item border border-base-300">
     <input type="radio" name="my-accordion-4" />
-    <div class="collapse-title text-xl font-medium">Add some questions here</div>
+    <div class="collapse-title text-xl font-medium">Will this application help me with my fitness journey/goals?</div>
     <div class="collapse-content">
-      <p>Answer</p>
+      <p>Our application is here to help faciliate the OSU FSF Events posted for users. We provide the tools you need to help get you be successful in your journey!</p>
     </div>
   </div>
   
